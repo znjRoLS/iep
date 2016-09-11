ae75e2b4d29c6b206d28b4a94abe00f2d68da9fe
rols <rols@DESKTOP-OVBSHS6>
2016-09-11 11:45:02 -0700

    Add project files.

---
+++ iep-newer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iep_newer.Startup))]
+namespace iep_newer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
